31202482c94ddf38d2131120e4419b6ead50dfa8
Askhat.Sam <askhat.sam@gmail.com>
2023-01-15 16:27:10 +0600

    feat: create goolge map url

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position){
         const {latitude, longitude}=position.coords;
-        console.log(latitude, longitude);
+        console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
     }, function(){
         alert('Could not get your position')
     })
