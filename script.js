a90a54203db69707c1b59bec6f2b07e8f40f35fe
Askhat.Sam <askhat.sam@gmail.com>
2023-01-19 21:37:21 +0600

    feat: count the number of clicks on workout form

--- script.js
+++ script.js
@@ -20,16 +20,16 @@ class Workout {
         this.distance=distance;
         this.duration=duration;
         
-}
-_setDescription(){
-    //prettier-ignore
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    }
+    _setDescription(){
+        //prettier-ignore
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-    this.description=`${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`
-}
-click(){
-    this.click++
-}
+        this.description=`${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`
+    }
+    click(){
+        this.clicks++
+    }
 }
 
 
